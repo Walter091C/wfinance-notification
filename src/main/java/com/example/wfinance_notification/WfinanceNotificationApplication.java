86a61000705dafab8d131b695f30205e22eb1bb2
Walter | Corporativo <walterjuniorcorporativo@gmail.com>
2026-06-05 13:06:50 -0300

    Atualizando branch remota

---
+++ src/main/java/com/example/wfinance_notification/WfinanceNotificationApplication.java
@@ -0,0 +1,13 @@
+package com.example.wfinance_notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WfinanceNotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WfinanceNotificationApplication.class, args);
+	}
+
+}
